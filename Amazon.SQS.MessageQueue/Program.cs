132c722525673d7207f9bcf23fb95b990d6516a7
Scott Vaughan <scott@agidea.uk>
2018-04-17 11:16:23 +0100

    Add menu

--- Amazon.SQS.MessageQueue/Program.cs
+++ Amazon.SQS.MessageQueue/Program.cs
@@ -18,8 +18,53 @@ namespace Amazon.SQS.MessageQueue
 
         private static void Start()
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Enter command:");
+                Console.WriteLine(Environment.NewLine +
+                                  "(1) Create Queue " + Environment.NewLine +
+                                  "(2) Get Queue Url " + Environment.NewLine +
+                                  "(3) Send Message " + Environment.NewLine +
+                                  "(4) Receive Messages " + Environment.NewLine +
+                                  "(5) Delete Messages " + Environment.NewLine +
+                                  "(6) Delete Queue " + Environment.NewLine +
+                                  "(0) Exit " + Environment.NewLine);
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    throw new ArgumentNullException();
+                }
+
+                switch (input.ToUpper())
+                {
+                    case "1":
+                        //CreateQueue();
+                        break;
+                    case "2":
+                        //GetQueueUrl();
+                        break;
+                    case "3":
+                        //SendMessage();
+                        break;
+                    case "4":
+                        //ReceiveMessages();
+                        break;
+                    case "5":
+                        //DeleteMessages();
+                        break;
+                    case "6":
+                        //DeleteQueue();
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Invalid menu selection.");
+                        continue;
+                }
+
+                break;
+            }
         }
     }
 }
