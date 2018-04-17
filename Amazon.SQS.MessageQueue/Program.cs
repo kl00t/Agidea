08a5a277fef823f877d5e216af45976e170c5e81
Scott Vaughan <scott@agidea.uk>
2018-04-17 11:13:46 +0100

    Add Ninject

--- Amazon.SQS.MessageQueue/Program.cs
+++ Amazon.SQS.MessageQueue/Program.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using Ninject;
 
 namespace Amazon.SQS.MessageQueue
 {
     public class Program
     {
         public static void Main(string[] args)
+        {
+            Setup();
+            Start();
+        }
+
+        private static void Setup()
+        {
+            IKernel kernel = new StandardKernel();
+        }
+
+        private static void Start()
         {
             Console.WriteLine("Hello World!");
             Console.ReadLine();
