4816422b81e8d489a4ec740a3c1f6710284ebafb
Scott Vaughan <scott@agidea.uk>
2018-04-17 11:10:27 +0100

    Add nuget packages

--- Amazon.SQS.MessageQueue/Program.cs
+++ Amazon.SQS.MessageQueue/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Amazon.SQS.MessageQueue
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
